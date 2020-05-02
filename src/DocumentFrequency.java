45230562ae32c0a2db50fdee0108c675baa996f1
James Slomka <james.slomka@ssense.com>
2020-05-01 22:20:17 -0400

    remove useless comments

--- src/DocumentFrequency.java
+++ src/DocumentFrequency.java
@@ -10,7 +10,7 @@ import java.util.HashSet;
 public class DocumentFrequency {
 
     public static void main(String[] args) {
-        String dir = "../docs";// name of directory with input files
+        String dir = "../docs";
         HashMap<String, Integer> dfs;
         dfs = extractDocumentFrequencies(dir, 40);
         writeDocumentFrequencies(dfs,"../output/frequency.txt");
@@ -24,18 +24,14 @@ public class DocumentFrequency {
     *
     */
     public static HashMap<String, Integer> extractDocumentFrequencies(String directory, int nDocs) {
-        // Creating a new HashMap to store the (word, frequency)
         HashMap<String,Integer> wordFreq = new HashMap<String,Integer>();
-
         HashSet<String> allWords = new HashSet<String>();
 
         String dir = directory;
         for(int counter = 1; counter<= nDocs; counter++){
-            // Populating 'allWords' with the unique words found the directory (which increases by 1 each loop)
 
-            allWords = extractWordsFromDocument(dir +"/" + counter + ".txt");  // Method that returns a HashSet of all the unique words in a document
+            allWords = extractWordsFromDocument(dir +"/" + counter + ".txt");
 
-            // Creating array list of all the unique words found in the file
             String words[] = allWords.toString().replaceAll("[^a-zA-Z ']", "").toLowerCase().trim().split(" ");
 
             // Adding all words from the first file to the HashMap
@@ -43,7 +39,7 @@ public class DocumentFrequency {
                 for(int c = 0; c < allWords.size();c++){
                     wordFreq.put(words[c],1);
                 }
-            }    // Method that returns a HashSet of all the unique words in a document
+            }
             else{
                 for(int c = 0; c < allWords.size();c++){
                     // If a pre-existing word is found, increment the value
@@ -62,7 +58,6 @@ public class DocumentFrequency {
         return wordFreq;
     }
 
-    // Method that returns a HashSet of all the unique words in a document
     public static HashSet<String> extractWordsFromDocument(String filename) {
 
         HashSet<String> words = new HashSet<String>();
@@ -74,14 +69,10 @@ public class DocumentFrequency {
             while(line != null){
 
                 line = normalize(line);
-
-                // Splitting the words up after a spacefilename
                 String [] temp = line.split(" ");
 
                 for(int counter = 0; counter <temp.length; counter ++){
-                    // Trimming the leading and following whitespace
                     temp[counter] = temp[counter].trim();
-                    // Omitting any blank entries and adding correct        // Declaring new HashSet entry to HashSet
                     if(temp[counter].equals("") || temp[counter].equals(" ")  || temp[counter].equals("'")){
                         continue;
                     }
@@ -96,12 +87,11 @@ public class DocumentFrequency {
             fr.close();
         }
         catch(IOException e){
-            System.out.println("*** IO e    // Method that returns a HashSet of all the unique words in a documentrror! [While reading] ***");
+            System.out.println("*** IO Errror - Reading File ***");
         }
         return words;
     }
 
-    // Writes document frequncy to file
     public static void writeDocumentFrequencies(HashMap<String, Integer> dfs, String filename) {
 
         try{
@@ -112,29 +102,23 @@ public class DocumentFrequency {
             Arrays.sort(temp);
             for(int counter = 0;counter<temp.length;counter++) {
                 bw.write(temp[counter].replaceAll("=", " ") + "\n");
-                //System.out.println(temp[counter].replaceAll("=", " "));
             }
 
             String words[] = dfs.toString().replaceAll("[^a-zA-Z=']", "").toLowerCase().trim().split("=");
             String numbers[] = dfs.toString().replaceAll("[^0-9,]", "").toLowerCase().trim().split(",");
 
-
-            for(int counter = 0; counter < words.length; counter++){
-                // Working code:
-                // bw.write(words[counter] + " " + numbers[counter] + "\n");
-
-                // Code below is formatted to columns
-                // bw.write(String.format("%-30s %-10s%n",words[counter] , numbers[counter] ));
-            }
+            // Code below is formatted to columns
+            // for(int counter = 0; counter < words.length; counter++){
+            //     bw.write(String.format("%-30s %-10s%n",words[counter] , numbers[counter] ));
+            // }
             bw.close();
             fw.close();
         }
         catch (IOException e ) {
-            System.out.println("*** IO error! [While writing] ***");
+            System.out.println("*** IO error! Writing File ***");
         }
     }
 
-    // Method to convert text to lowercase, as well as remove extra whitespace and punctuation
     public static String normalize(String word) {
         return word.replaceAll("[^a-zA-Z ']", "").toLowerCase();
     }
--- src/KeywordExtractor.java
+++ src/KeywordExtractor.java
@@ -16,21 +16,14 @@ public class KeywordExtractor {
             System.out.println("\n");
         }
         */
-
-        // This code allows you to run the // Method that returns a HashSet of all the unique words in a documentcode through the command interface
-        // i.e. run KeywordExractor
-
         for(int counter =1; counter<=40;counter++) {
             System.out.println(counter +".txt");
             HashMap<String, Double> j = computeTFIDF(computeTermFrequencies("../docs/"+counter+".txt"), readDocumentFrequencies("../output/frequency.txt"), 40);
             printTopKeywords(j, 5);
             System.out.print("\n");
         }
-
     }
 
-    // This method takes a file name as input and returns a HashMap of the term frequencies of each word
-    // (How many times a word is found in that document)
     public static HashMap<String, Integer> computeTermFrequencies(String filename) {
         HashMap<String,Integer> words = new HashMap<String,Integer>();
 
@@ -60,17 +53,13 @@ public class KeywordExtractor {
             }
         }computeTermFrequencies
         catch(IOException e){
-            System.out.println("IO ERROR! [While Reading]");
+            System.out.println("IO ERROR! Reading File");
         }
         return words;
     }
 
-    // This method reads a frequency file and returns a HashMap of the document frequencies of words
-    // Reads "freqs.txt" and stores data in HashMap
     public static HashMap<String, Integer> readDocumentFrequencies(String filename) {
         HashMap<String,Integer> wordFreq = new HashMap<String,Integer>();
-        // Tacking on .txt to filename
-        //filename = filename +".txt";
 
         try{
             FileReader fr = new FileReader(filename);
@@ -78,7 +67,6 @@ public class KeywordExtractor {
             String line = br.readLine();
 
             while(line != null){
-                // 2 variables: 1 to hold the words, and 1 for the numbers
                 String tempWords = line.replaceAll("[^a-zA-Z ']","").trim();
                 int tempNum = Integer.parseInt(line.replaceAll("[^0-9]","").trim());
                 wordFreq.put(tempWords,tempNum);
@@ -90,7 +78,7 @@ public class KeywordExtractor {
 
         }
         catch(IOException e ){
-            System.out.println("*** IO error! [While reading] ***");
+            System.out.println("*** IO error! Reading ***");
         }
 
         return wordFreq;
@@ -98,48 +86,36 @@ public class KeywordExtractor {
 
     public static HashMap<String, Double> computeTFIDF(HashMap<String, Integer> tfs, HashMap<String, Integer> dfs, double nDocs){
         // TF-IDF score: (w, d) = tf(w, d) / idf(w)
-
         HashMap<String,Double> score = new HashMap<String,Double>();
 
-        //Calculating TF score:
-        //Array to hold the frequency of each term in the input file
         int[] tfval = new int[tfs.size()];
 
         int counter =0;
         for(String key: tfs.keySet()){computeTermFrequencies
            int val = tfs.get(key);
             tfval[counter] = val;
-            //System.out.println(tfval[counter]);
             counter ++;
         }
-        //Array to hold the each term in the input file
         String[] tfkey = new String[tfs.size()];
 
         int count =0;
         for(String key: tfs.keySet()){// Method that returns a HashSet of all the unique words in a document
             tfkey[count] = key;
-           // System.out.println(tfkey[count]);
             count ++;
         }
 
         // Now we have tfkey which holds the terms, and tfval which holds their corresponding times
         // that it is found in a given document
-
-        //Calculating the IDF Score:
         double[] IDF = new double[tfs.size()];
 
-        //Array to hold the IDF scores
         for(int c =0; c < tfs.size(); c++){
             if(dfs.get(tfkey[c]) != null){
                 IDF[c] = Math.log(nDocs/dfs.get(tfkey[c]));
-               // System.out.println(IDF[c]);
             }
         }
 
-        //Calculating the TF- IDF score
         for(int j =0; j < IDF.length;j++) {
             double temp = tfval[j] * IDF[j];
-            //System.out.println(temp);
             score.put(tfkey[j],temp);
         }
 
